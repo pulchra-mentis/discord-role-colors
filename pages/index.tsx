8ce107d64f903b6f1741ff068585f9f025460c91
Nausicaä Hathor <thoughthaven@gmail.com>
2021-09-10 00:00:41 -0400

    Remove commented import statements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
 import { useState } from 'react'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-// import iro from '@jaames/iro'
 import Color from 'color'
 import { SketchPicker } from 'react-color'
 
